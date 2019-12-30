5b02091f5c0029f0f9e5de5764debe823f270647
Jatan Dewgun <43433361+jdewgun@users.noreply.github.com>
2019-12-30 21:13:38 +0100

    Create running_time_complexity.py

---
+++ 30DaysofCode/running_time_complexity.py
@@ -0,0 +1,26 @@
+import math
+
+def check_prime(x): 
+    if x <= 1: 
+        return False
+    elif x == 2: 
+        return True
+    elif (x > 2 and x % 2 == 0): 
+        return False
+    md = math.floor(math.sqrt(x)) 
+    for i in range(3, 1 + md, 2): 
+        if (x % i == 0): 
+            return False
+    return True
+
+x = int(input())
+
+a=[]
+
+for i in range(0,x):
+    a.append(int(input()))
+for i in a:
+    if (check_prime(i)):
+        print("Prime")
+    else:
+        print("Not prime")
