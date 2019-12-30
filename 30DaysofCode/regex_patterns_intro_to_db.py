a2a0a09d7664e63ca451ed13a75007dc93e4b427
Jatan Dewgun <43433361+jdewgun@users.noreply.github.com>
2019-12-30 21:55:31 +0100

    Create regex_patterns_intro_to_db.py

---
+++ 30DaysofCode/regex_patterns_intro_to_db.py
@@ -0,0 +1,10 @@
+import re
+
+list_of_names = []
+
+for i in range(int(input())):
+    first_name, emails = [str(string) for string in input().split()]
+    if re.search('@gmail\.com$', emails):
+           list_of_names.append(first_name)
+
+print(*sorted(list_of_names), sep='\n')
