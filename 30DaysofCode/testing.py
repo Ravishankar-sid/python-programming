05b476f22ca4c6ed5f4799152371ff806066fc2a
Jatan Dewgun <43433361+jdewgun@users.noreply.github.com>
2019-12-30 21:48:22 +0100

    Create testing.py

---
+++ 30DaysofCode/testing.py
@@ -0,0 +1,42 @@
+from random import randint
+
+class TestDataEmptyArray(object):
+   
+    @staticmethod
+    def get_array():
+        return []
+
+class TestDataUniqueValues(object):
+    _data = set()
+    while len(_data) < 10:
+        _data.add(randint(0, 100))
+
+    @staticmethod
+    def get_array():
+        data = TestDataUniqueValues._data
+        return list(data)
+        
+
+    @staticmethod
+    def get_expected_result():
+        data = TestDataUniqueValues.get_array()
+        return data.index(min(data))
+
+class TestDataExactlyTwoDifferentMinimums(object):
+
+    _data = set()
+    while len(_data) < 10:
+        _data.add(randint(0, 100))
+
+    _new_data = list(_data)
+    _new_data.append(min(_new_data))
+
+    @staticmethod
+    def get_array():
+        data =  TestDataExactlyTwoDifferentMinimums._new_data
+        return data
+
+    @staticmethod
+    def get_expected_result():
+        data = TestDataExactlyTwoDifferentMinimums.get_array()
+        return data.index(min(data))
