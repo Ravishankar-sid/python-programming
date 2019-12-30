63a986cdb425651407174ec5ea346a21f8964933
Jatan Dewgun <43433361+jdewgun@users.noreply.github.com>
2019-12-30 22:10:37 +0100

    Create operators.py

---
+++ 30DaysofCode/operators.py
@@ -0,0 +1,9 @@
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost*(tip_percent/100)
+    tax = meal_cost*(tax_percent/100)
+    
+    total_cost = meal_cost + tip + tax
+
+    total_cost = round(total_cost)
+
+    return total_cost
