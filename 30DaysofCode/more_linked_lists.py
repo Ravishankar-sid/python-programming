1eafc7796ad8516e5c0fc3ec69d4e2d6ba756913
Jatan Dewgun <43433361+jdewgun@users.noreply.github.com>
2019-12-30 21:08:15 +0100

    Create more_linked_lists.py

---
+++ 30DaysofCode/more_linked_lists.py
@@ -0,0 +1,8 @@
+def removeDuplicates(self,head):
+        _cursor = head
+        while _cursor:
+            while _cursor.next and _cursor.data == _cursor.next.data:
+                _cursor.next = _cursor.next.next
+            _cursor = _cursor.next
+
+        return head
