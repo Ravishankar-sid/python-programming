9ea63dd242b3b80b347c020361bd880a8339607d
Jatan Dewgun <43433361+jdewgun@users.noreply.github.com>
2019-12-30 23:19:56 +0100

    Create queues_and_stacks.py

---
+++ 30DaysofCode/queues_and_stacks.py
@@ -0,0 +1,13 @@
+class Solution:
+    def __init__(self):
+        self.stack = list()
+        self.queue = list()
+
+    def pushCharacter(self, char):
+          self.stack.append(char)
+    def enqueueCharacter(self, char):
+          self.queue.append(char)
+    def popCharacter(self):
+          return self.stack.pop(-1)
+    def dequeueCharacter(self):
+          return self.queue.pop(0)
