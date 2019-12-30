8682560e524477f394d4990e686a81701868cb13
Jatan Dewgun <43433361+jdewgun@users.noreply.github.com>
2019-12-30 21:59:42 +0100

    Create generics.swift

---
+++ 30DaysofCode/generics.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+struct Printer<T> {
+	/**
+	*    Name: printArray
+	*    Print each element of the generic array on a new line. Do not return anything.
+	*    @param A generic array
+	**/
+	
+	func printArray<Int>(array: [Int]) {
+    for a in array{
+        print(a)
+    }
+    }
+    
+}
